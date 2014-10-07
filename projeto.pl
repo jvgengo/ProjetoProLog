4d805447124d794d54d2bdc5e0f7eebd4f24f3ed
jvgengo <jvgengo@gmail.com>
2014-10-07 13:53:50 -0300

    exercicio2 funcionando

--- projeto.pl
+++ projeto.pl
@@ -17,7 +17,7 @@ materia(7,arquitetura_computadores,6).
 materia(8,microcontroladores,4).
 
 %curriculo(cc,[cm1,cm2,...,cmn]).
-curriculo(1,[1,2,3,4]).
+curriculo(1,[2,3]).
 curriculo(2,[5,6,7,8]).
 curriculo(3,[1]).
 
@@ -34,7 +34,7 @@ cursa(12189,2).
 cursa(12179,3).
 
 % historico (ra,[i1,i2,....,in])
-historico(12176,[item(1,1,2012,3.0,0.77),item(1,2,2013,6.5,0.90),item(5,1,2014,8.0,0.80)]).
+historico(12176,[item(3,1,2012,5.5,0.77),item(2,2,2013,6.5,0.90),item(5,1,2014,8.0,0.80)]).
 historico(12189,[item(1,1,2012,7.0,0.80),item(2,2,2013,8.5,0.80),item(3,1,2014,5.0,0.75)]).
 historico(12123,[item(5,1,2012,6.0,0.70),item(5,2,2013,7.5,0.90),item(6,1,2014,5.0,0.90)]).
 historico(12789,[item(7,1,2012,6.0,0.75),item(8,2,2014,8.0,0.89)]).
@@ -43,10 +43,10 @@ historico(12179,[item(1,2,2000,9,0.8),item(5,2,2000,9,0.8)]).
 % predicado que retorna a lista dos codigos de materias aprovadas
 materias_aprovadas(RA,LISTA_R):-historico(RA,A),materias_aprovadas_aux(A,LISTA_R).
 materias_aprovadas_aux([],[]).
-materias_aprovadas_aux([item(CM,_,_,N,F)|R],[CM|NOVO_R]):-N@>=5,F@>=0.75,materias_aprovadas_aux(R,NOVO_R).
-materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@>=5,F@<0.75,materias_aprovadas_aux(R,NOVO_R).
-materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@<5,F@>=0.75,materias_aprovadas_aux(R,NOVO_R).
-materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@<5,F@<0.75,materias_aprovadas_aux(R,NOVO_R).
+materias_aprovadas_aux([item(CM,_,_,N,F)|R],[CM|NOVO_R]):-N@>=5.0,F@>=0.75,materias_aprovadas_aux(R,NOVO_R).
+materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@>=5.0,F@<0.75,materias_aprovadas_aux(R,NOVO_R).
+materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@<5.0,F@>=0.75,materias_aprovadas_aux(R,NOVO_R).
+materias_aprovadas_aux([item(_,_,_,N,F)|R],NOVO_R):-N@<5.0,F@<0.75,materias_aprovadas_aux(R,NOVO_R).
 
 % predicado que retorna a lista dos codigos de materias cursadas
 materias_cursadas(RA,LISTA_R):-historico(RA,A),materias_cursadas_aux(A,LISTA_R).
@@ -62,10 +62,6 @@ removeOcorrencias(_,[],[]).
 removeOcorrencias(X,[X|R],NR):-removeOcorrencias(X,R,NR).
 removeOcorrencias(X,[Y|R],[Y|NR]):-X\==Y,removeOcorrencias(X,R,NR).
 
-%predicado que retorna o nome das matérias de uma lista de Cod de cursos
-nomeMaterias([],[]).
-nomeMaterias([item(CM,_,_,N,_)|R],[N|NOVO_R]):-nomeMaterias(R,NOVO_R).
-
 %======================================================================
 % 1) Descobrir se um aluno, cujo RA ser� fornecido, concluiu o curso
 % cujo c�digo tambem ser fornecido; execu��o pretendida:
@@ -75,28 +71,32 @@ nomeMaterias([item(CM,_,_,N,_)|R],[N|NOVO_R]):-nomeMaterias(R,NOVO_R).
 tem(X,[X|_]).
 tem(X,[P|R]):-X\==P,tem(X,R).
 
-contem_lista([],[]).
+contem_lista([],_).
 contem_lista([X|R],LISTA):-tem(X,LISTA),contem_lista(R,LISTA).
 
 concluiu(RA,CC):-materias_aprovadas(RA,LISTA_R),curriculo(CC,LISTA_C),contem_lista(LISTA_C,LISTA_R).
 
-%=====================================================================
+%======================================================================
 % 2) Produzir a lista dos nomes das mat�rias que faltam para um aluno,
-% cujo RA ser� fornecido, concluir o curso, cujo c�digo tamb�m ser�
+% cujo RA sera fornecido, concluir o curso, cujo codigo tambem sera
 % fornecido (a lista produzida deve ser retornada em um terceiro
-% par�metro); execucaoo pretendida:
+% parametro); execucaoo pretendida:
 %======================================================================
 
-falta(RA,CC,OQUE):-materias_aprovadas(RA,LISTA_R),curriculo(CC,LISTA_C),removeLista(LISTA_R,LISTA_C,S),nomeMaterias(S,N).
+%predicado que retorna o nome das matérias de uma lista de Cod de cursos
+nomeMaterias([],[]).
+nomeMaterias([CM|R],[NOME|NOVO_R]):-materia(CM,NOME,_),nomeMaterias(R,NOVO_R).
+
+falta(RA,CC,OQUE):-materias_aprovadas(RA,LISTA_R),curriculo(CC,LISTA_C),removeLista(LISTA_R,LISTA_C,LISTA_N_CONCLUIDAS),nomeMaterias(LISTA_N_CONCLUIDAS,OQUE).
 
 %======================================================================
 % 3) Produzir a lista dos nomes das mat�rias extra curriculares cursadas
 % pelo aluno, cujo RA ser� fornecido (o c�digo do curso a ser considerado
-% ser� fornecido em um segundo par�metro e a lista produzida deve ser
+% sera fornecido em um segundo par�metro e a lista produzida deve ser
 % retornada em um terceiro par�metro); execu��o pretendida:
 %======================================================================
 
-extra(RA,CC,LISTA_R):-materiasCursadas(RA,LISTA_M),curriculo(CC,LISTA_C),removeLista(LISTA_C,LISTA_M,S),nomeMaterias(S,N).
+%extra(RA,CC,LISTA_R):-materiasCursadas(RA,LISTA_M),curriculo(CC,LISTA_C),removeLista(LISTA_C,LISTA_M,S),nomeMaterias(S,N).
 
 %======================================================================
 % 4) Calcular quantos por cento das mat�rias obrigat�rias para a
